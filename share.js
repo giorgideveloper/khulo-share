055e4610eff732c41654aa0137b513ac78013e19
jorjodeveloper <gorgiladze888@gmail.com>
2025-10-29 17:27:26 +0400

    fix

---
+++ share.js
@@ -0,0 +1,26 @@
+export default function handler(req, res) {
+  const { title, description, image, url } = req.query;
+
+  const html = `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta property="og:title" content="${title}" />
+    <meta property="og:description" content="${description}" />
+    <meta property="og:image" content="${image}" />
+    <meta property="og:url" content="${url}" />
+    <meta property="og:type" content="website" />
+    <title>${title}</title>
+  </head>
+  <body>
+    <script>
+      window.location.href = "${url}";
+    </script>
+  </body>
+  </html>
+  `;
+
+  res.setHeader("Content-Type", "text/html");
+  res.send(html);
+}
